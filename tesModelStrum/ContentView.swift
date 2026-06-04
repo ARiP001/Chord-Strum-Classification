c4caae568be0b81146769162f4fc82bf4c763e05
Arif Fathurrahman <ariffathurrahman0@gmail.com>
2026-06-04 14:20:16 +0700

    change

--- tesModelStrum/ContentView.swift
+++ tesModelStrum/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack(spacing: 40) {
-            Text("🎶 Strum Master ganti")
+            Text("🎶 Strum Master ganti lagu 🎶")
                 .font(.system(size: 34, weight: .black, design: .rounded))
             
             // Area Permainan Visual
