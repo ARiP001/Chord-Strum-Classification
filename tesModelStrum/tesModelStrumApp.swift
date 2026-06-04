e8d98a34c5b027d5c5f81549b2d0ca3caa084f87
Arif Fathurrahman <ariffathurrahman0@gmail.com>
2026-06-04 13:34:34 +0700

    Initial Commit

---
+++ tesModelStrum/tesModelStrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  tesModelStrumApp.swift
+//  tesModelStrum
+//
+//  Created by Arif Fathurrahman on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct tesModelStrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
